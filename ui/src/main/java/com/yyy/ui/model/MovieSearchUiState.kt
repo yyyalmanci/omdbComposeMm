98c312c023e868a2c18161dfe73a9d4ad2785218
yyyalmanci <y.y.y-19@hotmail.com>
2025-05-23 21:24:44 +0300

    add filters

--- ui/src/main/java/com/yyy/ui/model/MovieSearchUiState.kt
+++ ui/src/main/java/com/yyy/ui/model/MovieSearchUiState.kt
@@ -11,10 +11,26 @@ data class MovieSearchUiState(
     val isLoading: Boolean = false,
     val showFilmNotFound: Boolean = false,
     val searchHistory: List<String> = emptyList(),
-    val favoriteMovieIds: Set<FavoriteMovieId> = emptySet()
+    val favoriteMovieIds: Set<FavoriteMovieId> = emptySet(),
+    val typeFilter: String? = null,
+    val yearFilter: String? = null,
+    val sortOption: SortOption = SortOption.NONE
 )
 
 data class FavoriteMovieId(
     val imdbId: String,
     val listTitle: String
 )
+
+enum class MovieType(val type: String) {
+    MOVIE("movie"),
+    SERIES("series"),
+    EPISODE("episode")
+}
+
+enum class SortOption {
+    NONE,
+    YEAR,
+    A_TO_Z,
+    Z_TO_A
+}
